45f7a9a32304c9cecefc8168449f1b9df48e4ea6
valdo <gkorchun@gmail.com>
2022-12-13 14:51:55 +0200

    Added states and events

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 import TodoListItem from '../todo-list-item';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({
+	todos,
+	...actions
+}) => {
 
 	const elements = todos.map((item) => {
 		const { id, ...itemProps } = item;
 
 		return (
 			<li key={ id } className="list-group-item">
-				<TodoListItem { ...itemProps } />
+				<TodoListItem
+					{ ...itemProps }
+					onToggleDone={ () => actions.onToggleDone(id) }
+					onToggleImportant={ () => actions.onToggleImportant(id) }
+					onItemDelete={ () => actions.onItemDelete(id) } />
 			</li>
 		);
 	});
