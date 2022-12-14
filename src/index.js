06b9061de5ecc3d80cf8998da405a6e6f3904c97
valdo <gkorchun@gmail.com>
2022-12-14 15:48:58 +0200

    revert-all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './scss/main.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from "./components/app";
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
